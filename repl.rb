fd11afc5a3c8a6b0ded4445cd652215232cd745e
Martin Manelli <manelli.ml@gmail.com>
2015-06-23 23:10:41 -0300

    Use Readline for REPL

--- repl.rb
+++ repl.rb
@@ -1,15 +1,18 @@
 require './plis'
 require './scheme_env'
 
+require 'readline'
+
 local_env = scheme_env
 
 loop do
   begin
-    print 'plis> '
-    line = gets.chomp
+    line = Readline.readline('plis> ', true)
+
+    exit if line =~ /^exit$/i
+
     puts plis_eval(parse(line), local_env)
   rescue Interrupt
-    puts "\nBye!"
     exit
   end
 end
